94434158cd3ccdb1b5fd398e81b8b0936c3a1dc2
mcarceles <mcarceles@tuenti.com>
2020-03-13 23:57:04 +0100

    Hydrated rows

--- script/novum.js
+++ script/novum.js
@@ -13,12 +13,19 @@ function getLevels(cells) {
         }
     });
 
+    let asArray = Object.entries(levels);
+    asArray.forEach((entry, index) => {
+        let [key, start] = entry;
+        let end = (index < asArray.length - 1) ? levels[asArray[index + 1][0]] : cells.length
+        levels[key] = { start: start, end: end }
+    })
+
     console.log("Levels are", levels)
     return levels;
 }
 
 function getCompetenciesRow(cells, levels) {
-    let level1Row = levels['1'],
+    let level1Row = levels['1'].start,
         currentRow = level1Row,
         previous;
 
@@ -41,10 +48,48 @@ function getCompetencies(row) {
     return competencies;
 }
 
+function hydrateCompetencies(cells, levels, competencies) {
+
+    let competenciesRow = getCompetenciesRow(cells, levels)
+
+    for (let [key, competency] of Object.entries(competencies)) {
+
+        // Description
+        competency.description = cells[competenciesRow + 1][competency.column]
+
+        // Milestones
+        competency.milestones = competency.milestones || [];
+        Object.values(levels).forEach(level => {
+            let levelDescription = level.start,
+                levelDetails = level.start + 2
+
+            let milestone = {
+                summary: "",
+                signals: [],
+                examples: []
+            };
+            milestone.summary = cells[level.start][competency.column]
+            for (let row = levelDetails; row < level.end; row++) {
+                let behaviour, practice
+                if (behaviour = cells[row][competency.column]) {
+                    milestone.signals.push(behaviour)
+                }
+                if (practice = cells[row][competency.column]) {
+                    milestone.examples.push(practice)
+                }
+            }
+            console.log("Milestone", milestone);
+            competency.milestones.push(milestone)
+        })
+    }
+
+    console.log("Competencies", competencies)
+}
+
 function generalExecutingRows(sheets) {
     sheets.spreadsheets.values.get({
         spreadsheetId: SHEET_ID,
-        range: 'General - Executing!A1:X',
+        range: 'Executing!A1:X',
     }, (err, res) => {
         if (err) return console.log('The API returned an error: ' + err);
         const cells = res.data.values;
@@ -55,6 +100,8 @@ function generalExecutingRows(sheets) {
             //Fetch Competencies
             let competenciesRow = getCompetenciesRow(cells, levels);
             let competencies = getCompetencies(cells[competenciesRow]);
+
+            hydrateCompetencies(cells, levels, competencies);
 
         } else {
             console.log('No data found.');
